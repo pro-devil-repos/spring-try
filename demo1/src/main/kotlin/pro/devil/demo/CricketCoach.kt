ea858e9a0d50003ce326d819c36f5c9360dde75f
Vyacheslav Tilikov <devil.ster.1@gmail.com>
2020-09-08 12:23:42 +0300

    Добавим DI через сеттеры (проперти в контексте Kotlin) для литералов

--- demo1/src/main/kotlin/pro/devil/demo/CricketCoach.kt
+++ demo1/src/main/kotlin/pro/devil/demo/CricketCoach.kt
@@ -2,9 +2,12 @@ package pro.devil.demo
 
 class CricketCoach : ICoach {
 
+    var emailAddress: String = ""
+    var team: String = ""
+
     lateinit var fortuneService: IFortuneService
 
-    override fun getDailyWorkout() = "Play some cricket!"
+    override fun getDailyWorkout() = "Play some cricket! With your Team - ${this.team} ($emailAddress)"
 
     override fun getDailyFortune(): String {
         if (this::fortuneService.isInitialized) {
