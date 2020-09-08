fdf3f53d53692250ceec1e52437e5f1f53bdbbf9
Vyacheslav Tilikov <devil.ster.1@gmail.com>
2020-09-08 15:16:46 +0300

    IoC на аннотациях с явным указанием названия бина

--- demoSpringAnnot/src/main/kotlin/pro/devil/demo/BaseballCoach.kt
+++ demoSpringAnnot/src/main/kotlin/pro/devil/demo/BaseballCoach.kt
@@ -1,5 +1,8 @@
 package pro.devil.demo
 
+import org.springframework.stereotype.Component
+
+@Component("myCoach")
 class BaseballCoach(private val fortuneService: IFortuneService? = null) : ICoach {
     override fun getDailyWorkout() = "Потрать 30 минут на тренировку отбивания мяча"
 
--- demoSpringAnnot/src/main/kotlin/pro/devil/demo/mainSpringApp.kt
+++ demoSpringAnnot/src/main/kotlin/pro/devil/demo/mainSpringApp.kt
@@ -1,5 +1,13 @@
 package pro.devil.demo
 
-fun main() {
+import org.springframework.context.support.ClassPathXmlApplicationContext
 
+fun main() {
+    val context = ClassPathXmlApplicationContext("applicationContext.xml")
+
+    val coach = context.getBean("myCoach", ICoach::class.java)
+
+    println(coach.getDailyWorkout())
+
+    context.close()
 }
