22feb271b45277e575ce75abf847826f3a353350
Vyacheslav Tilikov <devil.ster.1@gmail.com>
2020-09-08 13:24:32 +0300

    Initial

---
+++ demoSpringAnnot/build.gradle.kts
@@ -0,0 +1,17 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.0"
+}
+group = "pro.devil"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+}
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "13"
+}
