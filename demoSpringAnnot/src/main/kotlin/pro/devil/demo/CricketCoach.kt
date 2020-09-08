d12aafaa87ee20e83abd0dc3bdf023e9c0f93e46
Vyacheslav Tilikov <devil.ster.1@gmail.com>
2020-09-08 16:19:59 +0300

    Иньекция литеральных значений из файла

--- demoSpringAnnot/src/main/kotlin/pro/devil/demo/CricketCoach.kt
+++ demoSpringAnnot/src/main/kotlin/pro/devil/demo/CricketCoach.kt
@@ -2,16 +2,23 @@ package pro.devil.demo
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class CricketCoach : ICoach {
 
+    @Value("\${foo.email}")
+    private lateinit var email: String
+
+    @Value("\${foo.team}")
+    private lateinit var team: String
+
     @Autowired
     @Qualifier("sadFortuneService")
     lateinit var fortuneService: IFortuneService
 
-    override fun getDailyWorkout() = "Play some cricket! With your Team"
+    override fun getDailyWorkout() = "Play some cricket! With your Team - $team ($email)"
 
     override fun getDailyFortune(): String {
         if (this::fortuneService.isInitialized) {
